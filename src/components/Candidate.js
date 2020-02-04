551be9a2fdbaf68a6c33916aa6a5ba8830456833
Tina Fun <tinafun77@gmail.com>
2020-02-04 16:56:35 -0500

    candidates rendering on front end

--- src/components/Candidate.js
+++ src/components/Candidate.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import { gql } from 'apollo-boost';
 import { graphql } from 'react-apollo';
 
+
+
+
 const getCandidateQuery = gql`
   {
     candidates {
       id
+      imgURL
       firstName
       lastName
+      intro
     }
   }
 `;
 
-class TestCandidates extends Component {
+
+
+class Candidate extends Component {
   displayCandidates() {
     let data = this.props.data;
     if (data.loading) {
@@ -20,18 +27,23 @@ class TestCandidates extends Component {
     } else {
       return data.candidates.map(candidate => {
         return (
-          <li key={candidate.id}>
-            {candidate.firstName} {candidate.lastName} {candidate.id}
-          </li>
+          <div key={candidate.id}>
+            <img src={candidate.imgURL} alt="candidate img"/>
+            <h5>{candidate.firstName} {candidate.lastName}</h5>
+            <p>{candidate.intro}</p>
+          </div>
         );
       });
     }
   }
   render() {
-    console.log('this.props', this.props.data);
-
-    return <div>{this.displayCandidates()}</div>;
+    return (
+       
+            <div>
+               <div> {this.displayCandidates()}</div>
+            </div>
+    )
   }
 }
 
-export default graphql(getCandidateQuery)(TestCandidates);
+export default graphql(getCandidateQuery)(Candidate);
