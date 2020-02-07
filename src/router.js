398c833844c714ac12f3f095cd59d2e8be555696
Remi mendoza <47014638+mendozaremi@users.noreply.github.com>
2020-02-07 12:07:25 -0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -8,15 +8,12 @@ import LandingPage from './components/LandingPage';
 import CompanyOrCandidate from './components/CompanyOrCandidate';
 import Candidates from './components/candidate/Candidate';
 import Companies from './components/company/Companies';
-<<<<<<< HEAD
 import UserLogin from './components/Login/UserLogin/UserLogin';
 import CandidateLogin from './components/Login/CandidateLogin/CandidateLogin';
 import LoginSelector from './components/Login/LoginSelector/LoginSelector';
-=======
 import CompanyPositions from './components/company/CompanyPositions';
 import TestComp from './components/company/testComp';
 import NewPositionForm from './components/forms/NewPositionForm';
->>>>>>> 4839622a2d1979f41fc13b8852fdac4ef2fb04ea
 
 class Router extends Component {
   constructor(props) {
@@ -28,7 +25,6 @@ class Router extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route path="/signup" component={CandidateLogin} />
@@ -36,25 +32,13 @@ class Router extends Component {
         <Route path="/companysignup" component={CompanyForm} />
         <Route path="/candidatesignup" component={CandidateForm} />
         <Route path="/candidates" component={Candidates} />
+        <Route path="/newPosition" component={NewPositionForm} />
+        <Route path="/positions" component={CompanyPositions} />
         <Route path="/companies" component={Companies} />
         <Route path="/login" component={LoginSelector} />
         <Route path="/candidatelogin" component={CandidateLogin} />
         <Route path="/userlogin" component={UserLogin} />
       </Switch>
-=======
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/signup" component={CompanyOrCandidate} />
-          <Route path="/companysignup" component={CompanyForm} />
-          <Route path="/candidatesignup" component={CandidateForm} />
-          <Route path="/newPosition" component={NewPositionForm} />
-          <Route path="/candidates" component={Candidates} />
-          <Route path="/companies" component={Companies} />
-          <Route path="/positions" component={CompanyPositions} />
-          <Route path="/fml" component={TestComp} />
-        </Switch>
-
->>>>>>> 4839622a2d1979f41fc13b8852fdac4ef2fb04ea
     );
   }
 }
