167d4c70e62891fddc7e274f2b7b0f7770761883
Depak Borhara <depakborhara@gmail.com>
2020-02-07 11:23:01 -0500

    Master merge fixes

--- src/router.js
+++ src/router.js
@@ -5,12 +5,12 @@ import { Switch, Route } from 'react-router-dom';
 import CompanyForm from './components/forms/CompanyForm';
 import CandidateForm from './components/forms/CandidateForm';
 import LandingPage from './components/LandingPage';
-import CompanyOrCandidate from './components/CompanyOrCandidate'
-import Candidates from './components/candidate/Candidate'
-import Companies from './components/company/Companies'
-import CompanyPositions from './components/company/CompanyPositions'
-import TestComp from './components/company/testComp'
-import NewPositionForm from './components/forms/NewPositionForm'
+import CompanyOrCandidate from './components/CompanyOrCandidate';
+import Candidates from './components/candidate/Candidate';
+import Companies from './components/company/Companies';
+import CompanyPositions from './components/company/CompanyPositions';
+import TestComp from './components/company/testComp';
+import NewPositionForm from './components/forms/NewPositionForm';
 
 class Router extends Component {
   constructor(props) {
@@ -22,17 +22,17 @@ class Router extends Component {
 
   render() {
     return (
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/user" component={CompanyOrCandidate} />
-          <Route path="/companysignup" component={CompanyForm} />
-          <Route path="/candidatesignup" component={CandidateForm} />
-          <Route path="/newPosition" component={NewPositionForm} />
-          <Route path="/candidates" component={Candidates} />
-          <Route path="/companies" component={Companies} />
-          <Route path="/positions" component={CompanyPositions} />
-          <Route path="/fml" component={TestComp} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/user" component={CompanyOrCandidate} />
+        <Route path="/companysignup" component={CompanyForm} />
+        <Route path="/candidatesignup" component={CandidateForm} />
+        <Route path="/newPosition" component={NewPositionForm} />
+        <Route path="/candidates" component={Candidates} />
+        <Route path="/companies" component={Companies} />
+        <Route path="/positions" component={CompanyPositions} />
+        <Route path="/fml" component={TestComp} />
+      </Switch>
     );
   }
 }
