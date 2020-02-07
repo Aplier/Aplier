97ba4c8ec3c4e1da6a83a775d3372ed51868d41c
Depak Borhara <depakborhara@gmail.com>
2020-02-07 16:08:45 -0500

    WorkingTemp

--- src/components/forms/newPositionForm.js
+++ src/components/forms/newPositionForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import {addCompanyPositionMutation} from '../../queries/queries'
+import { addCompanyPositionMutation } from '../../queries/queries';
 
 class NewPositionForm extends Component {
   constructor(props) {
@@ -24,10 +24,10 @@ class NewPositionForm extends Component {
         salaryRange: this.state.salarayRange,
         screeningQ1: this.state.screeningQ1,
         screeningQ2: this.state.screeningQ1,
-        screeningQ3: this.state.screeningQ1
+        screeningQ3: this.state.screeningQ1,
       },
     });
-    this.props.history.push('/')
+    this.props.history.push('/');
   }
 
   render() {
@@ -35,31 +35,43 @@ class NewPositionForm extends Component {
       <div>
         <p className="miniLogo">Aplier</p>
         <div className="formContainer">
-        <img className="circleCompany"
-          src="https://i.imgur.com/nP6CxET.png"
-          alt="companyImage"
-          /> <br />
+          <img
+            className="circleCompany"
+            src="https://i.imgur.com/nP6CxET.png"
+            alt="companyImage"
+          />{' '}
+          <br />
           <h3>Add Position</h3>
           <form onSubmit={this.onSubmit.bind(this)}>
-            <label className="Clabel">Position title</label> <br/>
+            <label className="Clabel">Position title</label> <br />
             <input
               className="Cinput"
               onChange={event => this.setState({ title: event.target.value })}
               value={this.state.title}
-            /> <br/> <br/>
-            <label className="Clabel">Description</label> <br/>
+            />{' '}
+            <br /> <br />
+            <label className="Clabel">Description</label> <br />
             <input
               className="Cinput"
-              onChange={event => this.setState({ description: event.target.value })}
+              onChange={event =>
+                this.setState({ description: event.target.value })
+              }
               value={this.state.description}
-            /> <br/> <br/>
+            />{' '}
+            <br /> <br />
             <label className="Clabel">Salary</label>
             <input
               className="Cinput"
-              onChange={event => this.setState({ salarayRange: event.target.value })}
+              onChange={event =>
+                this.setState({ salarayRange: event.target.value })
+              }
               value={this.state.salarayRange}
-            /> <br/><br/>
-            <button className="customeButton" type="submit">Add Position</button>
+            />{' '}
+            <br />
+            <br />
+            <button className="customeButton" type="submit">
+              Add Position
+            </button>
           </form>
         </div>
       </div>
