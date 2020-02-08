aad7c6d7469421527ee547d40784a0cced8e9c11
Remi Mendoza <mendozaremi@gmail.com>
2020-02-07 21:25:32 -0500

    fixMaster-thiscopy works completely

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 //components
 import Router from './router';
 // import Navbar from './components/UI/Navbar/Navbar';
+
 //Apollo Client
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
