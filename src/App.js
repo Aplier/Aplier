8d76ff2a580c177f633c4a96308b570369267c7c
Depak Borhara <depakborhara@gmail.com>
2020-02-10 11:09:45 -0500

    Hold

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import { ApolloClient } from 'apollo-boost';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
-// import { withAuthenticator } from 'aws-amplify-react';
 import { ApolloProvider } from 'react-apollo';
-
-//components
+// import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
+import { HttpLink, InMemoryCache, gql } from 'apollo-boost';
+// import { createHttpLink } from 'apollo-link-http';
 import Router from './router';
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:5000/graphql',
+});
+const client = new ApolloClient({
+  cache,
+  link,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors);
+    console.log('networkError', networkError);
+  },
+});
+//components
 // import Navbar from './components/UI/Navbar/Navbar';
 //Apollo Client
-const client = new ApolloClient({
-  link: { uri: 'http://localhost:5000/graphql' },
-  cache: {},
-});
+// const link = createHttpLink({ uri: 'http://localhost:5000/graphql' });
+// const client = new ApolloClient({
+//   uri: 'http://localhost:5000/graphql',
+// cache: new InMemoryCache(),
+// });
 
 Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
