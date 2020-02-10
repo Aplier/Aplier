1bdd7b58e15f3671e1ebddfdc97de3e5ed1a1080
mh-393 <55870690+mh-393@users.noreply.github.com>
2020-02-10 17:53:49 -0500

    candidatePositions query fixed

--- server/schema/employer/candidatePositions.js
+++ server/schema/employer/candidatePositions.js
@@ -8,8 +8,27 @@ const candidatePositions = `
     candidateId: ID
     companyPositionId: ID
   }
+
+  extend type Query {
+    positionApplied(candidateId: Int!): [CandidatePositions]
+  }
 `;
 
+const candidatePositionsResolvers = {
+  Query: {
+    positionApplied: (parent, args, { models }) => {
+      try{
+        return models.CandidatePositions.findAll({
+          where: args
+        });
+      }catch(err){
+        console.error(err);
+      }
+    }
+  }
+}
+
 module.exports = {
-  candidatePositions
+  candidatePositions,
+  candidatePositionsResolvers
 };
