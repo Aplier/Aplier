71dd2af10727d413d4832d67b04b79e6fa388aba
Remi Mendoza <mendozaremi@gmail.com>
2020-02-11 18:12:27 -0500

    depak fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
 //Auth
-import Amplify from 'aws-amplify';
+import Amplify , { Auth }from 'aws-amplify';
 import aws_exports from './Util/aws-exports';
 
 //components
@@ -11,16 +11,36 @@ import Router from './router';
 import Navbar from './components/Header/Navbar/Navbar';
 import SideDrawer from './components/Header/SideDrawer/SideDrawer';
 import Backdrop from './components/Header/Backdrop/Backdrop';
+import SideDrawerCandidate from './components/Header/SideDrawer/SideDrawerCandidate'
+import SideDrawerCompany from './components/Header/SideDrawer/SideDrawerCompany'
+
 
 //Apollo Client
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
+  uri: 'http://localhost:4000/graphql',
 });
 
 class App extends Component {
-  state = {
+  constructor(props) {
+    super(props)
+
+  this.state = {
     sideDrawerOpen: false,
+    isCandidateLoggedIn: false,
+    isUserLoggedIn: false
   };
+}
+
+  componentDidMount() {
+    console.log('WHAT IS BROKENNN')
+    if(Auth.currentAuthenticatedUser()){
+
+      this.setState({isCandidateLoggedIn: true})
+    }else {
+      this.setState({isCandidateLoggedIn: false})
+    }
+  }
+
 
   drawerToggleClickHandler = () => {
     this.setState(prevState => {
@@ -33,17 +53,39 @@ class App extends Component {
   };
 
   render() {
+
+    const {isCandidateLoggedIn, isUserLoggedIn} = this.state
+
+    const candidateSideDrawer = (props) => {
+      if(isCandidateLoggedIn){
+        return <SideDrawerCandidate show={this.state.sideDrawerOpen}/>
+      }else{
+        return <SideDrawer show={this.state.sideDrawerOpen}/>
+      }
+    }
+
+    const companySideDrawer = (props) => {
+      if(isUserLoggedIn){
+        return <SideDrawerCompany show={this.state.sideDrawerOpen}/>
+      }else{
+        return <SideDrawer show={this.state.sideDrawerOpen}/>
+      }
+    }
+
+
     Amplify.configure(aws_exports);
     let backdrop;
 
     if (this.state.sideDrawerOpen) {
       backdrop = <Backdrop click={this.backdropClickHandler} />;
     }
+
     return (
       <ApolloProvider client={client}>
         <div style={{ height: '100%' }}>
           <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
-          <SideDrawer show={this.state.sideDrawerOpen} />
+          {candidateSideDrawer()}
+          {companySideDrawer()}
           {backdrop}
           <Router />
         </div>
