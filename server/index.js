6da4de2dba8b864790b9a74cc2e150e4424838cc
Tina Fun <tinafun77@gmail.com>
2020-02-14 11:32:39 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sessionStore = new SequelizeStore({ db });
 const path = require('path');
 const { Client } = require('pg');
+const PORT = process.env.PORT || 4000
+
 const client = new Client({
   host: 'aplier.ccx1yvxrllrz.us-east-1.rds.amazonaws.com',
   user: 'Aplier',
@@ -19,34 +21,46 @@ const client = new Client({
 });
 client.connect();
 
+
+
+
 //CORS FOR DEV
-app.use(cors());
+// app.use(cors());
 
 const createApp = () => {
-  // body parsing middleware
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  // compression middleware
-  // app.use(compression());
-
-  // session middleware with passport
   app.use(
-    session({
-      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-      store: sessionStore,
-      resave: false,
-      saveUninitialized: false,
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+      context: { models },
     })
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
+  )
+}
+//   // body parsing middleware
+//   app.use(express.json());
+//   app.use(express.urlencoded({ extended: true }));
 
-  // auth and api routes
-  // app.use('/auth', require('./auth'));
+//   // compression middleware
+//   // app.use(compression());
 
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')));
+//   // session middleware with passport
+//   app.use(
+//     session({
+//       secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+//       store: sessionStore,
+//       resave: false,
+//       saveUninitialized: false,
+//     })
+//   );
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+
+//   // auth and api routes
+//   // app.use('/auth', require('./auth'));
+
+//   // static file-serving middleware
+//   app.use(express.static(path.join(__dirname, '..', 'public')));
 
   // any remaining requests with an extension (.js, .css, etc.) send 404
   app.use((req, res, next) => {
@@ -59,10 +73,6 @@ const createApp = () => {
     }
   });
 
-  // sends index.html
-  // app.use('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-  // });
 
   // error handling endware
   app.use((err, req, res, next) => {
@@ -70,21 +80,14 @@ const createApp = () => {
     console.error(err.stack);
     res.status(err.status || 500).send(err.message || 'Internal server error.');
   });
-};
+// };
 
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-    context: { models },
-  })
-);
+
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  app.listen(process.env.PORT || 4000, () => {
-    console.log(`server running`);
+  app.listen(PORT, () => {
+    console.log(`server running on ${PORT}`);
   });
 
   // set up our socket control center
